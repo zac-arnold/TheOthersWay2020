a49a4af264be9a2e71d7c8f46ffccc666442e2bc
Zac Arnold <videozac@gmail.com>
2020-08-05 17:30:37 +1200

    add bandcamp logo

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import fblogo from '../assets/images/Facebook.svg'
 import instalogo from '../assets/images/Instagram.png'
 import spotlogo from '../assets/images/Spotify.png'
-import TOWlogo from '../assets/images/TOW-logo-transparent.jpg'
 import Twitlogo from '../assets/images/Twitter.png'
-
+import BClogo from '../assets/images/bandcamp-button-bc-circle-black-512.png'
 
 export default function SocialMedia (props) {
   const { twitter, instagram, facebook, spotify, bandcamp } = props.socialMedia
@@ -14,10 +13,11 @@ export default function SocialMedia (props) {
       <a href={instagram}><img className="socialmedia" src={instalogo} alt="Instagram Logo" /></a>
       <a href={facebook}><img className="socialmedia" src={fblogo} alt="Facebook Logo" /></a>
       <a href={spotify}><img className="socialmedia" src={spotlogo} alt="Spotify Logo" /></a>
-      <a href={bandcamp}>Bandcamp</a>
+      <a href={bandcamp}><img className="socialmedia" src={BClogo} alt="Bandcamp Logo" /></a>
     </div>
   )
 }
+
 
 
 
