5c90ab7ee264bc9a714d5bdd73f76bfa7f6b544b
Zac Arnold <videozac@gmail.com>
2020-08-07 09:27:42 +1200

    add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import WebBanner from '../assets/images/WebsiteBanner2480x180.png'
+
+export default function Header() {
+  return (
+    <div>
+      <img src={WebBanner} alt="" />
+    </div>
+  )
+}
--- src/templates/Artist.jsx
+++ src/templates/Artist.jsx
@@ -5,11 +5,13 @@ import Bio from '../components/Bio'
 import PressPhoto from '../components/PressPhoto'
 import Youtube from '../components/Youtube'
 import SocialMedia from '../components/SocialMedia'
+import Header from '../components/Header'
 
 export default function Artist({ pageContext }) {
   const { name, bio, img, video, id, socialMedia } = pageContext
   return (
     <div className="tobytext">
+      <Header />
       <ArtistNav />
       <h1>{name}</h1>
       <Bio bio={bio} />
