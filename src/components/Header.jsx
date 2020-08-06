7aada51a5f9efebca9381f4a1a77bb9cf9bd055a
Zac Arnold <videozac@gmail.com>
2020-08-07 09:38:11 +1200

    css header width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import WebBanner from '../assets/images/WebsiteBanner2480x180.png'
 export default function Header() {
   return (
     <div>
-      <img src={WebBanner} alt="" />
+      <img className="header" src={WebBanner} alt="" />
     </div>
   )
 }
