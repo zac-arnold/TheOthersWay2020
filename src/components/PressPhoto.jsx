dff68394ea576c0cc95e7a4e6bbf46a98adec547
Zac Arnold <videozac@gmail.com>
2020-08-06 18:49:45 +1200

    debug img problem

--- src/components/PressPhoto.jsx
+++ src/components/PressPhoto.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
+
 export default function PressPhoto (props) {
-  
+  console.log('props.img - ', props.img)
   return (
     <div className="imageContainer">
       <img src={props.img} alt="" />
