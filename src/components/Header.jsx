64ec73cad20cae23baa5fe2aff2dd0033996c8c3
Zac Arnold <videozac@gmail.com>
2020-08-07 09:40:56 +1200

    add hyperlink to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,8 @@ import WebBanner from '../assets/images/WebsiteBanner2480x180.png'
 export default function Header() {
   return (
     <div>
-      <img className="header" src={WebBanner} alt="" />
+      <a href="https://flyingout.co.nz/products/the-others-way-2020" target="blank" rel="noopener noreferrer"><img className="header" src={WebBanner} alt="" /></a>
+      
     </div>
   )
 }
